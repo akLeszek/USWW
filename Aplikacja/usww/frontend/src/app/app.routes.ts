8a64c64bc1d63ecedf93ee20dc4922ce56086f93
Adrian <akleszek99@gmail.com>
2025-03-10 22:38:19 +0100

    Implementacja komponentu zmiany haseł

--- Aplikacja/usww/frontend/src/app/app.routes.ts
+++ Aplikacja/usww/frontend/src/app/app.routes.ts
@@ -1,7 +1,14 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+import {ChangePasswordComponent} from './auth/change-password/change-password.component';
+import {authGuard} from './auth/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'auth/change-password',
+    component: ChangePasswordComponent,
+    canActivate: [authGuard]
+  },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
